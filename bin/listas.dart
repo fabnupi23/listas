525d0ee078109bd55f79cf9f80d9c3ac7fa494a7
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2024-02-06 23:06:53 -0500

    Listas en Dart

---
+++ bin/listas.dart
@@ -0,0 +1,7 @@
+import 'package:listas/listas.dart' as listas;
+
+void main() {
+  //arrays, list.
+  //En medio de estos simbolos <> indicamos el tipo de dato con el que trabajaremos en la lista.
+  var names = <String>['Fabian', 'Andres'];
+}
---
+++ test/listas_test.dart
@@ -0,0 +1,8 @@
+import 'package:listas/listas.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
